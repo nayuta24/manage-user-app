526ae1b08e6291126dc3ceb10afdcb625d6398f8
NMYuta <yutanakamu824@gmail.com>
2021-10-05 19:57:49 +0900

    ヘッダー作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import './App.css';
+import { ChakraProvider} from "@chakra-ui/react";
+import { BrowserRouter } from 'react-router-dom';
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 function App() {
   return (
     <div className="App">
-      
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ChakraProvider>
     </div>
   );
 }
